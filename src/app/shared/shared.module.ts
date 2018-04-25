3cd6e4e7d0e308950c04948aa31268ee18451948
Braulio <braulio.rod.men@gmail.com>
2018-04-25 02:09:36 -0500

    Modulos modificados

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+@NgModule({
+    declarations: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+        NotFoundComponent
+    ],
+    exports: [
+        HeaderComponent,
+        SidebarComponent,
+        BreadcrumbsComponent,
+        NotFoundComponent
+    ]
+})
+
+export class ShareModule { }
+
