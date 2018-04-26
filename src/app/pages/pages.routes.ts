beaea7529715c1f6f1e5c80f0c5254e4776b03a1
Braulio <braulio.rod.men@gmail.com>
2018-04-26 01:52:44 -0500

    Breadcrumbs-observable, metatags

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -13,10 +13,10 @@ const pagesRoutes: Routes = [
     { path: '',
       component: PagesComponent,
       children: [
-          { path: 'dashboard', component: DashboardComponent },
-          { path: 'progress', component: ProgressComponent },
-          { path: 'graficas1', component: Graficas1Component },
-          { path: 'settings', component: SettingsComponent },
+          { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard'} },
+          { path: 'progress', component: ProgressComponent, data: { title: 'Progress'} },
+          { path: 'graficas1', component: Graficas1Component, data: { title: 'Graficas'} },
+          { path: 'settings', component: SettingsComponent, data: { title: 'Ajustes'} },
           { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
       ]
     }
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+
+
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,8 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  label: string = '';
+  constructor(
+    private router: Router,
+    public _title: Title,
+    public meta: Meta
+  ) {
+    this.getDataRoute()
+    .subscribe( data => {
+      this.label = data.title;
+      _title.setTitle(this.label);
 
-  constructor() { }
+      let metatag: MetaDefinition = {
+        name: 'description',
+        content: this.label
+      };
+
+      this.meta.updateTag( metatag );
+    });
+   }
+
+   getDataRoute() {
+    return this.router.events
+            .filter( event => event instanceof ActivationEnd && event.snapshot.firstChild === null )
+            .map((event: ActivationEnd) => event.snapshot.data);
+   }
 
   ngOnInit() {
   }
